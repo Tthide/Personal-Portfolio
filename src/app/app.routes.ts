5d35ff0cc2951dcb2859914a11580e3af6d0dc61
Tthide <Tthide@users.noreply.github.com>
2025-09-02 12:59:28 +0200

    feat: basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
+import { AboutComponent } from './about/about';
+import { ResumeComponent } from './resume/resume';
+import { ContactComponent } from './contact/contact';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent },
   // Add other routes here
 ];
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   template: `
-    <p>
-      header works!
-    </p>
+    <nav>
+      <a routerLink="/">Home</a>
+      |
+      <a routerLink="/about">About</a>
+            |
+      <a routerLink="/resume">Resume</a>
+            |
+      <a routerLink="/contact">Contact</a>
+    </nav>
+    <router-outlet />
   `,
   styles: ``
 })
