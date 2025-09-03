e4ca6e20434661b21c3b0bc88c0751deecf2cede
Tthide <113419806+Tthide@users.noreply.github.com>
2025-09-03 16:56:38 +0200

    feat: added mobile responsiveness

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,CommonModule],
   templateUrl: `./header.html`,
   styles: []
 })
-export class HeaderComponent { }
+export class HeaderComponent { 
+    isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
