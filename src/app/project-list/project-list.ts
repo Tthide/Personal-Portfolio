93837117224acc6d2e9672fcc8e709af68ef12a8
Tthide <bordethibault@gmail.com>
2025-09-09 10:49:54 +0200

    feat: colored tag project infocard

--- src/app/project-list/project-list.ts
+++ src/app/project-list/project-list.ts
@@ -6,12 +6,29 @@ import { DataService, Project } from '../services/data.service';
   selector: 'app-project-list',
   imports: [RouterModule],
   templateUrl: './project-list.html',
-  styles: ``
+  styles: ``,
+  host: {
+    'class': 'w-full flex flex-col items-center gap-10 block'
+  }
 })
 export class ProjectListComponent implements OnInit {
   projects: Project[] = [];
 
   constructor(private dataService: DataService) { }
+
+  chipColors = [
+    'bg-purple-600',
+    'bg-blue-500',
+    'bg-green-500',
+    'bg-yellow-500',
+    'bg-pink-500'
+  ];
+
+  getChipColor(index: number): string {
+    // Rotate colors if more keywords than colors
+    //console.log(index);
+    return this.chipColors[index % this.chipColors.length];
+  }
 
   ngOnInit(): void {
     this.dataService.getProjects().subscribe(data => {
