449d19b7650ce566564208676ed2236b14ecb31d
Tthide <bordethibault@gmail.com>
2025-09-09 11:15:20 +0200

    feat: dynamic project item page

--- src/app/project-item/project-item.ts
+++ src/app/project-item/project-item.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService, Project } from '../services/data.service';
+import { Observable, defer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { RotatingTextIconComponent } from '../shared/rotating-text-icon/rotating-text-icon';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-item',
-  imports: [RotatingTextIconComponent],
+  imports: [RotatingTextIconComponent,AsyncPipe],
   templateUrl: './project-item.html',
   styleUrl: './project-item.css'
 })
 export class ProjectItemComponent {
-  id!: string;
+  project$!: Observable<Project | undefined>;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private dataService: DataService) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => this.id = params.get('id') ?? '');
-  }
+    this.project$ = this.route.paramMap.pipe(
+      map(params => params.get('id') ?? ''),
+      switchMap(id => defer(() => this.dataService.getProjectById(+id)))
+    );  }
 }
