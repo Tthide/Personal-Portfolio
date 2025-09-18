d4bfd8afc484c76e6cb3641d5e4b4d33f7a5f5d1
Tthide <bordethibault@gmail.com>
2025-09-18 22:10:53 +0200

    feat: hero rework

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProjectListComponent } from '../project-list/project-list';
+import { RotatingTextIconComponent } from '../shared/rotating-text-icon/rotating-text-icon';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule,ProjectListComponent],
+  imports: [RouterModule,ProjectListComponent,RotatingTextIconComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
